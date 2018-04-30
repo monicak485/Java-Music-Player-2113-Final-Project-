c6c4e53ee8d0ebc98ec22e6396e5e6f97511b9d0
Gen-Gowanlock <38627254+Gen-Gowanlock@users.noreply.github.com>
2018-04-30 11:39:19 -0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,3 +1,10 @@
+/*////////////////////////////////
+Software Engineering Final Project
+Grace Gowanlock, Monica Kavathekar,
+and Abia Khan
+Professor James
+4/29/18
+////////////////////////////////*/
 import java.io.IOException;
 import java.io.*;
 import java.net.*;
@@ -9,55 +16,33 @@ class Server {
 
     while (true){
       try {
-        DatagramSocket ds = new DatagramSocket(10001);
-        DatagramSocket dssend = new DatagramSocket();
+        DatagramSocket ds = new DatagramSocket(10001); //this socket recieves info
+        DatagramSocket dssend = new DatagramSocket(); //this socket sends info
 
-              byte[] buffer = new byte[1024];
-              byte[] data = new byte[1024];
-                // prepare the buffer to hold the received data
-                //byte[] buffer = new byte[1024];
-                // create a new data packet based on the buffer
-                DatagramPacket dp = new DatagramPacket(buffer, buffer.length, InetAddress.getLocalHost(), 10001);
-                // receive data packet
-                ds.receive(dp);
-                // analyze the received data
-                data = dp.getData();
-
-
-                System.out.println("data: "+ new String(data).trim());
-
-                InetAddress address = dp.getAddress();
-
-                dp = new DatagramPacket(buffer, buffer.length, address, 10000);
-
-                dssend.send(dp);
-
-                //DatagramPacket dptest = new DatagramPacket(buffer, buffer.length, InetAddress.getLocalHost(), 10000);
-                //dssend.send(dptest);
-                /*
-                System.out.println("packet length: "+dp.getLength());
-                System.out.println("sender port number: "+dp.getPort());
-                System.out.println("sender address: "+dp.getAddress().toString());
-                */
+        byte[] buffer = new byte[1024]; //this stores the song info
+        byte[] data = new byte[1024]; //this also stores song info
+        
+        // prepare the buffer to hold the received data
+        // create a new data packet based on the buffer
+        DatagramPacket dp = new DatagramPacket(buffer, buffer.length, InetAddress.getLocalHost(), 10001);
+        // receive data packet
+        ds.receive(dp);
+        // analyze the received data
+        data = dp.getData();
 
 
-        //ds.close();
+        System.out.println("data: "+ new String(data).trim()); //prints song data to console
+
+        InetAddress address = dp.getAddress();
+
+        dp = new DatagramPacket(buffer, buffer.length, address, 10000); //packet contains song data with new port
+
+        dssend.send(dp); //sends packet back to playlist
+        
       } catch (IOException ex){
 
       }
-      //listenerSocket.receive(message);
-
-
-      /*
-      UDPSenderThread mythread3 = new UDPSenderThread();
-      UDPRecieverThread2 mythread4 = new UDPRecieverThread2();
-
-      Thread thread4 = new Thread(mythread3);
-      Thread thread2 = new Thread(mythread4);
-
-      thread4.start();
-      thread2.start();
-      */
+      
     }
   }
 }
